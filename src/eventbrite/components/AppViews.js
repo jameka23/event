7c24733a8602efce1ec5ab746b02b8c232a8d049
Jameka Echols <jeechols@aosmith.com>
2019-12-18 14:56:39 -0600

    modifications

--- src/eventbrite/components/AppViews.js
+++ src/eventbrite/components/AppViews.js
@@ -1,44 +1,47 @@
 /* eslint-disable no-unused-vars */
-import React, { Component } from "react";
+import React, { useState, useContext } from "react";
 import { Route } from "react-router-dom";
 import { withRouter } from "react-router";
 import Home from "./home/Home";
 import Login from "./authentication/Login";
 import EventbriteAPI from "./modules/eventbriteManager";
 import Register from "./authentication/Register";
-import * as firebase from "firebase";
 
-export default class AppViews extends Component {
-  state = {
-    users : [],
-    categories: [],
-    activeUser : Number(sessionStorage.getItem("userId"))
-  };
+import * as firebase from "firebase";
+import { createContext } from "vm";
+
+
+export const AuthContext = createContext(null);
+
+const AppViews = () => {
+
+  const [isLoggedIn, setLoggedIn] = useState(false);
   
 
-  componentDidMount(){
-    // get a reference to the firebase database
-    const db = firebase.database().ref().child('users');
+  // componentDidMount(){
+  //   // get a reference to the firebase database
+  //   const db = firebase.database().ref().child('users');
     
 
-    db.on('value', snapshot => {
-      //console.log(snapshot.val())
-      this.setState({
-        // snapshot.val is an obj that has key/value pairs that are obj that we want
-        users: Object.values(snapshot.val())
-      }) 
+  //   db.on('value', snapshot => {
+  //     //console.log(snapshot.val())
+  //     this.setState({
+  //       // snapshot.val is an obj that has key/value pairs that are obj that we want
+  //       users: Object.values(snapshot.val())
+  //     }) 
       
-    })
-    this.getCategories()
+  //   })
+  //   this.getCategories()
 
-  }
+  // }
 
   //check authentication
-  isAuthenticated = () => sessionStorage.getItem("credentials") !== null;
+  //isAuthenticated = () => sessionStorage.getItem("credentials") !== null;
 
 
   // make a call to get the categories of the events for user's to choose from 
-  getCategories = () =>{
+  // will need to change becauce eventbrite deprecated their event search api :(
+  const getCategories = () =>{
     EventbriteAPI.categories()
     .then(catObj =>{
       //console.log(catObj)
@@ -50,10 +53,10 @@ export default class AppViews extends Component {
 
 
   // This component will handle a bunch of the routing to the other components
-  render() {
+
     console.log(this.state.users)
     return (
-      <>
+      <AuthContext.Provider value={{isLoggedIn, setLoggedIn}}>
         <Route exact path="/home" render={ (props) => {
             return <Home 
                 {...props} 
@@ -72,9 +75,9 @@ export default class AppViews extends Component {
         <Route exact path="/register" render={(props) =>{
           return <Register {...props}/>
         }} />
-      </>
+      </AuthContext.Provider>
     );
-  }
+
 }
 
 withRouter(AppViews);
